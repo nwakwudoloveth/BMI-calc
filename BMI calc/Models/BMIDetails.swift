b9904a338f5df6d42889e91c674a4ca7d93144a9
loveth <nwakwudoloveth@gmail.com>
2021-12-03 09:54:20 +0100

    Calculator Brain, BMI details

---
+++ BMI calc/Models/BMIDetails.swift
@@ -0,0 +1,15 @@
+//
+//  BMIDetails.swift
+//  BMI calc
+//
+//  Created by Loveth Nwakwudo on 12/3/21.
+//
+
+import UIKit
+
+struct BMIDetails {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
